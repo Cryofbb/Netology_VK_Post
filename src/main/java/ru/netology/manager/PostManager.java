0da551de79a9caf4addba765d3b42a9eda0ddc5e
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-06-15 13:05:58 +0300

    Added Manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] post;
+
+    public Post[] search(int ownerID, String domain, String query, boolean ownersOnly, short count, int offset, boolean extended) {
+        return null;
+    }
+
+    public void delete(int ownerID, int postID) {
+    }
+}
